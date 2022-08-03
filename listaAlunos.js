1b7f0a2fca84d75dd95da6b4adea894ac11aa489
fulano de tal <gustavomartinsgs@outlook.com>
2022-08-02 22:26:20 -0300

    ATIVIDADE-ENCONTRO-REMOTO-2

---
+++ listaAlunos.js
@@ -0,0 +1,21 @@
+
+let numeroDeAlunos = 10;
+
+for (let contador = 0; contador < numeroDeAlunos; contador++) {
+
+    console.log(contador)
+
+    if (contador == 0) {
+        console.log ('classificando o zero')
+        
+    }
+
+         if (contador % 2 == 0) {
+            console.log('numero par')
+        
+        }else{
+            console.log('numero impar')
+        }
+
+}
+
